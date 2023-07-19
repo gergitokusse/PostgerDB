f3702b99c0fcf6da00520960782ff55ca811040b
Gergito Kusse <gergito2@gmail.com>
2023-07-19 18:19:56 +0300

    Project 2 query

--- queries.sql
+++ queries.sql
@@ -7,3 +7,40 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = 'true';
 SELECT * FROM animals WHERE name != 'Gebmon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 and weight_kg <= 17.3;
+
+-- Project-2
+BEGIN;
+UPDATE animals set species = 'unspecified';
+select * from animals;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals set species = 'digimon' WHERE name like '%mon';
+UPDATE animals set species = 'pokemon' WHERE species = '';
+select * from animals;
+COMMIT;
+select * from animals;
+
+BEGIN;
+DELETE FROM animals;
+select * from animals;
+ROLLBACK;
+select * from animals;
+
+SAVEPOINT P1;
+UPDATE animals set weight_kg = weight_kg*-1;
+ROLLBACK TO SAVEPOINT P1;
+UPDATE animals set weight_kg = weight_kg*-1 WHERE weight_kg < 0;
+
+
+-- Write queries to answer the following questions
+SELECT COUNT(name) FROM animals;
+SELECT COUNT(name) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT MAX(escape_attempts), name FROM animals GROUP BY name;
+SELECT MAX(weight_kg), MIN(weight_kg) FROM animals WHERE species = 'digimon';
+SELECT MAX(weight_kg), MIN(weight_kg) FROM animals WHERE species = 'pokemon';
+
+SELECT AVG(escape_attempts) FROM animals WHERE species = 'digimon' and date_of_brith BETWEEN '1990-01-01' AND '2000-01-01';
+SELECT AVG(escape_attempts) FROM animals WHERE species = 'pokemon' and date_of_brith BETWEEN '1990-01-01' AND '2000-01-01';
+
--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 /* Database schema to keep the structure of entire database. */
 CREATE DATABASE vet_clinic;
 CREATE TABLE animals(id INT NOT NULL, name VARCHAR(30) NOT NULL, date_of_brith DATE NOT NULL, escape_attempts INT NOT NULL, neutered BOOLEAN NOT NULL, weight_kg DEC(11,2) NOT NULL);
-/* ALTER TABLE animals ADD species VARCHAR(20); */
+ALTER TABLE animals ADD species VARCHAR(20);
